23fc918a719675621ff64ba28c131b234d635a28
zrv587 <1058252138@qq.com>
2018-06-25 09:03:26 +0800

    图片服务器图片上传下载的配置

--- src/main/java/com/ssdb/controller/FdfsDemo.java
+++ src/main/java/com/ssdb/controller/FdfsDemo.java
@@ -17,12 +17,12 @@ public class FdfsDemo {
         try {
             // 7.使用StorageClient对象上传文件(图片)
             // 参数1：文件名，参数名：扩展名，不能包含"."，参数3：文件的元数据，保存文件的原始名、大小、尺寸等，如果没有可为null
-        String[] strings = getStorageClient().upload_file("C:\\Users\\Administrator\\Desktop\\杨幂.jpg", "jpg", null);
+        String[] strings = getStorageClient().upload_file("C:\\Users\\Administrator\\Desktop\\meinv.jpg", "jpg", null);
         for (String str: strings ) {
             System.out.println(str);
         }
 
-       int i= downloadFile("group1","M00/00/00/wKgABFssufCAbI42AATU3yDSBmo385.jpg",new File("S.jpg"));
+       int i= downloadFile("group1","M00/00/00/wKgABFssye6AU92qAAEKu9v-PZM915.jpg",new File("meinv.jpg"));
             System.out.println(i);
         } catch (IOException e) {
             e.printStackTrace();
